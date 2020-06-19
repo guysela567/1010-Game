85a523cedbf6ed6140a59794d52dd6e9fb79c285
Harel243 <66994142+Harel243@users.noreply.github.com>
2020-06-19 18:28:01 +0300

    add class Cell

---
+++ game/Cell.js
@@ -0,0 +1,15 @@
+class Cell {
+
+    constructor(x, y, r, c) {
+      this.x = x;
+      this.y = y
+      this.r = r;
+      this.c = c;
+    }
+  
+    show() {
+      fill(this.c);
+      noStroke();
+      rect(this.x, this.y, this.r, this.r, 5);
+    }
+  }
