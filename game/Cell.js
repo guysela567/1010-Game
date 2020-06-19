d4a64176c3042669d34f5c984496f7aa1923c502
Harel243 <66994142+Harel243@users.noreply.github.com>
2020-06-19 21:40:53 +0300

    Update variable names

--- game/Cell.js
+++ game/Cell.js
@@ -1,15 +1,15 @@
 class Cell {
 
-    constructor(x, y, r, c) {
+    constructor(x, y, size, c) {
       this.x = x;
       this.y = y;
-      this.r = r;
+      this.size = size;
       this.c = c;
     }
   
     show() {
       fill(this.c);
       noStroke();
-      rect(this.x, this.y, this.r, this.r, 5);
+      rect(this.x, this.y, this.size, this.size, 5);
     }
   }
