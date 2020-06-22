1b13069273d117f554f111c269712165477452ea
guysela567 <guysela567@gmail.com>
2020-06-22 14:19:15 +0300

    added mouseHover function to cell

--- game/Cell.js
+++ game/Cell.js
@@ -10,4 +10,9 @@ class Cell {
       noStroke();
       rect(this.x, this.y, size, size, 5);
     }
+
+    mouseHover() {
+      return mouseX > this.x && mouseX < this.x + this.size
+      && mouseY > this.y && mouseY < this.y + this.size
+    }
   }
