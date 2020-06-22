46b730a3c61cc0ca94ba342a31dd32b9f4da7116
Harel243 <66994142+Harel243@users.noreply.github.com>
2020-06-22 14:12:53 +0300

    make isShapeFit function

--- game/grid-functions.js
+++ game/grid-functions.js
@@ -5,7 +5,13 @@ function tryAddToGrid(grid, x, y, shape) {
 }
 
 function isShapeFit(grid, x, y, shape) {
-    
+    for(let tile of shape.tiles){
+        const chosenTile = grid[x + tile.x][y + tile.y];
+        if(chosenTile.isEmpty){
+            return true;
+        }
+    }
+    return false;
 }
 
 function addShape(grid, x, y, shape) {
